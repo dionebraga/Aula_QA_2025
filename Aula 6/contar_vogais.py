fcc6555e2d71e74060b562716a3713f5caae62d1
Dione <dionebraga.work@gmail.com>
2025-10-22 17:51:38 -0300

    Atualizando arquivos vogais

---
+++ Aula 6/contar_vogais.py
@@ -0,0 +1,11 @@
+# Contabilizar vogais - Desafio do Caique
+
+palavra = input("Digite uma palavra: ").lower()
+vogais = "aeiou"
+contador = 0
+
+for letra in palavra:
+    if letra in vogais:
+        contador += 1
+
+print(f"A palavra '{palavra}' contém {contador} vogais.")
---
+++ Aula 6/verificar_email.py
@@ -0,0 +1,6 @@
+email = input("Digite seu e-mail institucional: ")
+
+if "@jogajuntoinstituto.org" in email:
+    print("✅ E-mail válido! Acesso liberado.")
+else:
+    print("❌ E-mail inválido. Utilize o e-mail institucional do Joga Junto.")
